afb760611fd2dcd0b911aa6e50534380bf748375
Irish Micole Cando <irishmicolecando@gmail.com>
2023-05-23 08:22:58 +0800

    Implement sign up page

---
+++ app/src/main/java/com/example/h2gether/Signup.kt
@@ -0,0 +1,11 @@
+package com.example.h2gether
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Signup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
