5361ef7b0a0c0a077e6fdca1842f82cc7a4cad9d
John Ric Merque <johnricmer@gmail.com>
2023-05-24 01:39:22 +0800

    added backend code for sign up activity

--- app/src/main/java/com/example/h2gether/Signup.kt
+++ app/src/main/java/com/example/h2gether/Signup.kt
@@ -1,11 +1,48 @@
 package com.example.h2gether
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.h2gether.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Signup : AppCompatActivity() {
+
+    private lateinit var binding:ActivitySignupBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnSignup.setOnClickListener{
+            val email = binding.etInputEmail.text.toString()
+            val pass = binding.etInputPassword.text.toString()
+            val confirmPass = binding.etConfirmPassword.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                if (pass == confirmPass) {
+
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
     }
 }
