c76f78380514414e45068e4e961d6fd9569463a2
John Ric Merque <johnricmer@gmail.com>
2023-05-24 12:07:33 +0800

    Commented out interface bypass

--- app/src/main/java/com/example/h2gether/MainActivity.kt
+++ app/src/main/java/com/example/h2gether/MainActivity.kt
@@ -48,12 +48,12 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-    override fun onStart() {
-        super.onStart()
-
-        if(firebaseAuth.currentUser != null){
-            val intent = Intent(this, PrefaceActivity::class.java)
-            startActivity(intent)
-        }
-    }
+//    override fun onStart() {
+//        super.onStart()
+//
+//        if(firebaseAuth.currentUser != null){
+//            val intent = Intent(this, PrefaceActivity::class.java)
+//            startActivity(intent)
+//        }
+//    }
 }
