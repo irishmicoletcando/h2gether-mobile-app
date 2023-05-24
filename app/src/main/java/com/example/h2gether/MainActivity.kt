99769630c51e537a2bc0bce3a91681272fbfe5de
John Ric Merque <johnricmer@gmail.com>
2023-05-24 11:20:40 +0800

    Added backend code for sign in

--- app/src/main/java/com/example/h2gether/MainActivity.kt
+++ app/src/main/java/com/example/h2gether/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.h2gether
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.h2gether.databinding.ActivityMainBinding
+import com.example.h2gether.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.tvRegisterAccount.setOnClickListener{
+            val intent = Intent(this, Signup::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnSignin.setOnClickListener{
+            val email = binding.etInputEmail .text.toString()
+            val pass = binding.etInputPassword.text.toString()
+
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, PrefaceActivity ::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
     }
 }
