01dab2254d1bab3ba199dba1c47e2c10a6612a41
Irish Micole Cando <irishmicolecando@gmail.com>
2023-06-04 18:29:16 +0800

    Set up preface pages

--- app/src/main/java/com/example/h2gether/fragments/FamilyMonitoringPrefacePageFragment.kt
+++ app/src/main/java/com/example/h2gether/fragments/FamilyMonitoringPrefacePageFragment.kt
@@ -7,12 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.h2gether.R
 
-class FirstFragment : Fragment() {
+class FamilyMonitoringPrefacePageFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_family_monitoring_preface_page, container, false)
     }
 }
