6f2b859a0e7a25f1ccdeffe52b7c825c6c050774
John Ric Merque <johnricmer@gmail.com>
2023-06-04 11:48:22 +0800

    Added dependencies for Google sign in

--- app/src/main/java/com/example/h2gether/LoginActivity.kt
+++ app/src/main/java/com/example/h2gether/LoginActivity.kt
@@ -4,18 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.example.h2gether.databinding.ActivityMainBinding
+import com.example.h2gether.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityLoginBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         firebaseAuth = FirebaseAuth.getInstance()
 
@@ -44,6 +44,10 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
 
             }
+        }
+
+        binding.btnGoogle.setOnClickListener{
+
         }
 
     }
