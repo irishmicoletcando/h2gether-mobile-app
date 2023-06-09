a4f593c13ccb3d559e72f0dd315bdcb326d38bc3
John Ric Merque <johnricmer@gmail.com>
2023-06-09 19:26:24 +0800

    Function to validate email and pass

--- app/src/main/java/com/example/h2gether/SignupActivity.kt
+++ app/src/main/java/com/example/h2gether/SignupActivity.kt
@@ -30,8 +30,8 @@ class SignupActivity : AppCompatActivity() {
             val confirmPass = binding.etConfirmPassword.text.toString()
 
             if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                validateInputs(email, pass)
                 if (pass == confirmPass) {
-
                     firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
                         if (it.isSuccessful) {
                             val intent = Intent(this, TrackWaterConsumptionOnboarding::class.java)
@@ -49,16 +49,39 @@ class SignupActivity : AppCompatActivity() {
             }
 
         }
-        // Function to validate email using regex pattern
-        fun isEmailValid(email: String): Boolean {
-            val pattern = Patterns.EMAIL_ADDRESS
-            return pattern.matcher(email).matches()
-        }
 
-        // Function to validate password
-        fun isPasswordValid(password: String): Boolean {
-            val passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$"
-            return password.matches(passwordRegex.toRegex())
+
         }
     }
+
+    // Function to validate email using regex pattern
+    fun isEmailValid(email: String): Boolean {
+        val pattern = Patterns.EMAIL_ADDRESS
+        return pattern.matcher(email).matches()
+    }
+
+    // Function to validate password
+    fun isPasswordValid(password: String): Boolean {
+        val passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$"
+        return password.matches(passwordRegex.toRegex())
+    }
+
+    // Function to display toast alert
+    fun showToastAlert(message: String) {
+        // Replace `context` with your actual Android context reference
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    // Usage example
+    fun validateInputs(email: String, password: String) {
+        if (!isEmailValid(email)) {
+            showToastAlert("Invalid email address")
+            return
+        }
+
+        if (!isPasswordValid(password)) {
+            showToastAlert("Invalid password. Password should be at least 8 characters long and contain at least one letter and one number.")
+            return
+        }
+        return
 }
