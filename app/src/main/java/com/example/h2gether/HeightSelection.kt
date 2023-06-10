7dbe515958633fac8ddf987a113be9c16804ea64
Irish Micole Cando <irishmicolecando@gmail.com>
2023-06-10 23:33:18 +0800

    Feat: Add user configuration pages

---
+++ app/src/main/java/com/example/h2gether/HeightSelection.kt
@@ -0,0 +1,32 @@
+package com.example.h2gether
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.h2gether.databinding.ActivityHeightSelectionBinding
+
+class HeightSelection : AppCompatActivity() {
+    private lateinit var binding: ActivityHeightSelectionBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHeightSelectionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.npHeight.minValue = 1
+        binding.npHeight.maxValue = 300
+
+        val btnNext = findViewById<Button>(R.id.btn_next)
+        btnNext.setOnClickListener {
+            val intent = Intent(this, ActivityLevelSelection::class.java)
+            startActivity(intent)
+        }
+
+        val btnBack = findViewById<Button>(R.id.btn_back)
+        btnBack.setOnClickListener {
+            val intent = Intent(this, WeightSelection::class.java)
+            startActivity(intent)
+        }
+    }
+}
