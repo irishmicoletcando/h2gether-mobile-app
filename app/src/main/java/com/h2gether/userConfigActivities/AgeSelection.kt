df32249aefd36d4b14db94682725f3474c0721a5
John Ric Merque <johnricmer@gmail.com>
2023-06-12 19:42:19 +0800

    Feat: Add age data to db

--- app/src/main/java/com/h2gether/userConfigActivities/AgeSelection.kt
+++ app/src/main/java/com/h2gether/userConfigActivities/AgeSelection.kt
@@ -4,31 +4,65 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import com.example.h2gether.R
 import com.example.h2gether.databinding.ActivityAgeSelectionBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class AgeSelection : AppCompatActivity() {
 
     private lateinit var binding: ActivityAgeSelectionBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAgeSelectionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        val uid = firebaseAuth.currentUser?.uid
+        var ageValue = 0
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("users/user-profile")
+
         binding.npAge.minValue = 1
         binding.npAge.maxValue = 100
 
-        val btnNext = findViewById<Button>(R.id.btn_next)
-        btnNext.setOnClickListener {
+        binding.btnNext.setOnClickListener {
+            if (uid != null) {
+                val newData: Map<String, Any> = mapOf(
+                    "age" to (ageValue)
+                )
+
+                val updates: MutableMap<String, Any> = HashMap()
+                updates["user$uid"] = newData
+
+                databaseReference.updateChildren(updates).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, AgeSelection::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "Failed to set age", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            }
+
             val intent = Intent(this, WeightSelection::class.java)
             startActivity(intent)
         }
 
-        val btnBack = findViewById<Button>(R.id.btn_back)
-        btnBack.setOnClickListener {
+        binding.btnBack.setOnClickListener {
             val intent = Intent(this, SexSelectionActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.npAge.setOnValueChangedListener { picker, oldVal, newVal ->
+            ageValue = binding.npAge.value
+
         }
     }
 }
