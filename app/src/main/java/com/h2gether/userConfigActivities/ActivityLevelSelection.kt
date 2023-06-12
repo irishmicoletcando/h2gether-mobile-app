68ec87a528ddb17824608a206505f0e3cc2fa9ba
John Ric Merque <johnricmer@gmail.com>
2023-06-12 21:01:35 +0800

    Feat: Database handlers for activity level data

--- app/src/main/java/com/h2gether/userConfigActivities/ActivityLevelSelection.kt
+++ app/src/main/java/com/h2gether/userConfigActivities/ActivityLevelSelection.kt
@@ -3,22 +3,83 @@ package com.h2gether.userConfigActivities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import com.example.h2gether.R
+import android.widget.Toast
+import com.example.h2gether.databinding.ActivityLevelSelectionBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class ActivityLevelSelection : AppCompatActivity() {
+    private lateinit var binding: ActivityLevelSelectionBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_level_selection)
+        binding = ActivityLevelSelectionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+        val uid = firebaseAuth.currentUser?.uid
 
-        val btnNext = findViewById<Button>(R.id.btn_next)
-        btnNext.setOnClickListener {
-            val intent = Intent(this, AgeSelection::class.java)
-            startActivity(intent)
+        databaseReference = FirebaseDatabase.getInstance().getReference("users/$uid/user-profile")
+
+        var activityLevelUserSelection: String? = null
+
+        binding.btnLightlyActive.setOnClickListener{
+            activityLevelUserSelection = "lightly active"
         }
 
-        val btnBack = findViewById<Button>(R.id.btn_back)
-        btnBack.setOnClickListener {
+        binding.btnModeratelyActive.setOnClickListener{
+            activityLevelUserSelection = "Moderately active"
+        }
+
+        binding.btnVeryActive.setOnClickListener{
+            activityLevelUserSelection = "Very active"
+        }
+
+        binding.btnSedentary.setOnClickListener{
+            activityLevelUserSelection = "Sedentary"
+        }
+
+        binding.btnNext.setOnClickListener {
+            if (activityLevelUserSelection != null) {
+                if (uid != null) {
+                    databaseReference.addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            val existingData: Map<String, Any>? = snapshot.value as? Map<String, Any>
+
+                            // Create a new map that includes the existing data and the new field
+                            val newData = existingData?.toMutableMap() ?: mutableMapOf()
+                            newData["activityLevel"] = activityLevelUserSelection!!
+
+                            databaseReference.updateChildren(newData)
+                                .addOnSuccessListener {
+                                    //
+                                }
+                                .addOnFailureListener { error ->
+                                    // Handle the failure
+                                }
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {
+                            TODO("Not yet implemented")
+                        }
+                    })
+
+                    val intent = Intent(this, AgeSelection::class.java)
+                    startActivity(intent)
+
+                }
+                else Toast.makeText(this, "Failed to set Activity Level", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Select activity level", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        binding.btnBack.setOnClickListener {
             val intent = Intent(this, HeightSelection::class.java)
             startActivity(intent)
         }
