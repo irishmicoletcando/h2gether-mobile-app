a4261204c9ca8caa88432f38bf60b0599febe221
Irish Micole Cando <irishmicolecando@gmail.com>
2023-06-13 20:32:29 +0800

    Feat: Add navigation bar

---
+++ app/src/main/java/com/example/h2gether/NavigationBarActivity.kt
@@ -0,0 +1,41 @@
+package com.example.h2gether
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.h2gether.databinding.ActivityNavigationBarBinding
+
+class NavigationBarActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityNavigationBarBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNavigationBarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(WaterDashboardPage())
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+
+            when(it.itemId) {
+                R.id.water_page -> replaceFragment(WaterDashboardPage())
+                R.id.statistics_page -> replaceFragment(StatisticsPage())
+                R.id.reminder_page -> replaceFragment(ReminderPage())
+                R.id.tips_page -> replaceFragment(TipsPage())
+
+                else -> {
+
+                }
+            }
+
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+}
