8e598e411a6d08c180b42c1d28c19cf2e1acb485
John Ric Merque <johnricmer@gmail.com>
2023-06-21 13:42:37 +0800

    Feat: Initial add water buttons handlers

--- app/src/main/java/com/h2gether/homePage/WaterDashboardPage.kt
+++ app/src/main/java/com/h2gether/homePage/WaterDashboardPage.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.h2gether.R
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import com.example.h2gether.databinding.FragmentWaterDashboardPageBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +23,9 @@ class WaterDashboardPage : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var selectedOption: Int? = null
+
+    private lateinit var binding: FragmentWaterDashboardPageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,15 +33,54 @@ class WaterDashboardPage : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentWaterDashboardPageBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_water_dashboard_page, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnAddWater.setOnClickListener {
+            Toast.makeText(context, "added $selectedOption ml", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.op50ml.setOnClickListener{
+            selectedOption = 50
+//            binding.op50ml.background = getDrawable(outValue.resourceId)
+//            binding.op100ml.background = getDrawable(R.drawable.option_select_bg)
+//            binding.op150ml.background = getDrawable(outValue.resourceId)
+//            binding.op200ml.background = getDrawable(outValue.resourceId)
+//            binding.op250ml.background = getDrawable(outValue.resourceId)
+//            binding.opCustom.background = getDrawable(outValue.resourceId)
+        }
+
+        binding.op100ml.setOnClickListener{
+            selectedOption = 100
+        }
+
+        binding.op150ml.setOnClickListener{
+            selectedOption = 150
+        }
+
+        binding.op200ml.setOnClickListener{
+            selectedOption = 200
+        }
+
+        binding.op250ml.setOnClickListener{
+            selectedOption = 250
+        }
+
+
+    }
+
 
     companion object {
         /**
