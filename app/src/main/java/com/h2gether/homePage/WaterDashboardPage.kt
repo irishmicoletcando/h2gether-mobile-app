d8091de74eb79e902c7521a7d1248e2cc8da8da1
John Ric Merque <johnricmer@gmail.com>
2023-06-26 23:19:57 +0800

    Feat: Retrofit instance using the base URL of the OpenWeatherMap API

--- app/src/main/java/com/h2gether/homePage/WaterDashboardPage.kt
+++ app/src/main/java/com/h2gether/homePage/WaterDashboardPage.kt
@@ -27,6 +27,8 @@ import com.google.gson.annotations.SerializedName
 import com.h2gether.userAuthActivites.LoginActivity
 import com.h2gether.userConfigActivities.WeightSelection
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 import java.text.SimpleDateFormat
@@ -276,7 +278,12 @@ class WaterDashboardPage : Fragment() {
     }
 
     private fun fetchWeather(){
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
+        
     }
 
     class WaterConsumptionDataModel {
