134cdf9f35aaaf4941cc169e5fe61f3adc299bd5
Irish Micole Cando <irishmicolecando@gmail.com>
2023-06-27 17:50:53 +0800

    Feat: Retrieve and display water consumption on LineChart

--- app/src/main/java/com/h2gether/homePage/StatisticsPage.kt
+++ app/src/main/java/com/h2gether/homePage/StatisticsPage.kt
@@ -1,60 +1,132 @@
 package com.h2gether.homePage
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.h2gether.R
+import android.widget.Toast
+import com.example.h2gether.databinding.FragmentStatisticsPageBinding
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.PropertyName
+import com.google.firebase.database.ValueEventListener
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [StatisticsPage.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StatisticsPage : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentStatisticsPageBinding
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var waterConsumed: Int? = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics_page, container, false)
+        binding = FragmentStatisticsPageBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StatisticsPage.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StatisticsPage().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fetchWaterDetails()
+    }
+
+    private fun fetchWaterDetails() {
+        firebaseAuth = FirebaseAuth.getInstance()
+        val uid = firebaseAuth.currentUser?.uid
+        val database: DatabaseReference = FirebaseDatabase.getInstance().reference
+        val dataRef: DatabaseReference = database.child("users/$uid/water-consumption")
+
+        dataRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    val waterConsumption = dataSnapshot.getValue(WaterConsumptionDataModel::class.java)
+
+                    if (waterConsumption != null) {
+                        waterConsumed = waterConsumption.waterConsumption
+
+                        // Process the retrieved data and create the entries and dates
+                        val entries = mutableListOf<Entry>()
+                        val dates = mutableListOf<String>()
+
+                        // Assuming waterConsumption is the value obtained from Firebase
+                        if (waterConsumption != null) {
+                            val currentDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
+                            val waterValue = waterConsumption.waterConsumption ?: 0
+                            entries.add(Entry(0f, waterValue.toFloat()))
+                            dates.add(currentDate)
+
+                            // Call the updateChart function with the processed data
+                            updateChart(entries, dates)
+                        }
+                    }
+                } else {
+                    Toast.makeText(context, "No data available", Toast.LENGTH_SHORT).show()
                 }
             }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle any errors that occur during retrieval
+            }
+        })
+    }
+
+    private fun updateChart(entries: List<Entry>, dates: List<String>) {
+        val chart = binding.lineChart
+        val dataSet = LineDataSet(entries, "Water Intake")
+        dataSet.color = Color.BLACK
+        dataSet.setCircleColor(Color.BLACK)
+        dataSet.lineWidth = 2f
+        dataSet.circleRadius = 4f
+        dataSet.setDrawCircleHole(false)
+        dataSet.valueTextSize = 10f
+        dataSet.setDrawFilled(true)
+        dataSet.fillColor = Color.BLUE
+        dataSet.fillAlpha = 50
+        dataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
+
+        val lineData = LineData(dataSet)
+
+        chart.description.isEnabled = false
+        chart.setTouchEnabled(false)
+        chart.isDragEnabled = false
+        chart.setScaleEnabled(false)
+        chart.setPinchZoom(false)
+
+        val xAxis = chart.xAxis
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.setDrawGridLines(false)
+        xAxis.granularity = 1f
+        xAxis.valueFormatter = IndexAxisValueFormatter(dates)
+
+        val yAxisLeft = chart.axisLeft
+        yAxisLeft.axisMinimum = 0f
+        yAxisLeft.axisMaximum = 0f
+        yAxisLeft.setDrawLimitLinesBehindData(true)
+
+        val yAxisRight = chart.axisRight
+        yAxisRight.isEnabled = false
+
+        chart.data = lineData
+        chart.invalidate()
+    }
+
+    class WaterConsumptionDataModel {
+        @PropertyName("waterConsumption")
+        var waterConsumption: Int? = 0
     }
 }
