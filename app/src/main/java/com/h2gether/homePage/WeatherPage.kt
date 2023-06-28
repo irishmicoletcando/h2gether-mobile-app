5b84735ea1656018f8ef05d29a74a6ca352fb4ae
Irish Micole Cando <irishmicolecando@gmail.com>
2023-06-28 22:58:54 +0800

    Feat: Add toolbar and fix page inflate

--- app/src/main/java/com/h2gether/homePage/WeatherPage.kt
+++ app/src/main/java/com/h2gether/homePage/WeatherPage.kt
@@ -9,35 +9,13 @@ import android.widget.ImageButton
 import android.widget.TextView
 import com.example.h2gether.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [WeatherPage.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WeatherPage : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val rootView = inflater.inflate(R.layout.fragment_tips_page, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_weather_page, container, false)
 
         val backButton = rootView.findViewById<ImageButton>(R.id.back_button)
         val pageTitle = rootView.findViewById<TextView>(R.id.toolbar_title)
@@ -50,24 +28,5 @@ class WeatherPage : Fragment() {
             //TODO: Handle back button click
         }
         return rootView
-    }
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ReminderPage.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WeatherPage().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
