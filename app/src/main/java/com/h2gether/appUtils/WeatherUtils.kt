12830f5414329af1c870ac829dc6983046cd2fea
John Ric Merque <johnricmer@gmail.com>
2023-06-30 08:30:51 +0800

    Refactor: Added separate file for weather utilities

---
+++ app/src/main/java/com/h2gether/appUtils/WeatherUtils.kt
@@ -0,0 +1,87 @@
+package com.h2gether.appUtils
+
+import android.content.ContentValues
+import android.util.Log
+import com.google.firebase.database.PropertyName
+import com.google.gson.annotations.SerializedName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+import java.util.Objects
+
+class WeatherUtils {
+    private fun fetchWeather(): WeatherResponse? {
+        val coroutineScope = CoroutineScope(Dispatchers.Main)
+        var weatherResponse: WeatherResponse? = null
+
+        // Call the fetchWeather function from a coroutine
+        coroutineScope.launch {
+            weatherResponse = fetchWeatherFromOpenWeather()
+
+        }
+
+        Log.i(ContentValues.TAG, weatherResponse.toString())
+        return weatherResponse
+    }
+
+    private suspend fun fetchWeatherFromOpenWeather(): WeatherResponse? {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val client = OpenWeatherMapApiClient(retrofit.create(OpenWeatherMapService::class.java))
+
+        return client.getCurrentWeather("Manila")
+
+    }
+
+    // Weather
+
+    class OpenWeatherMapApiClient(private val service: OpenWeatherMapService) {
+        suspend fun getCurrentWeather(location: String): WeatherResponse? {
+            val response = service.getCurrentWeather(location, "4d7436b2e953a39a0d36c842e7742e02")
+            Log.i(ContentValues.TAG, response.toString())
+            if (response.isSuccessful) {
+                return response.body()
+            }
+            return null
+        }
+    }
+
+    interface OpenWeatherMapService {
+        @GET("weather")
+        suspend fun getCurrentWeather(
+            @Query("q") location: String,
+            @Query("appid") apiKey: String
+        ): Response<WeatherResponse>
+    }
+
+    data class WeatherResponse(
+        @SerializedName("name")
+        val cityName: String,
+        @SerializedName("main")
+        val weatherData: WeatherData,
+        @SerializedName("weather")
+        val weatherDetails: List<WeatherDetails>
+    )
+
+    data class WeatherData(
+        @SerializedName("temp")
+        val temperature: Double,
+        @SerializedName("humidity")
+        val humidity: Double,
+        @SerializedName("feels_like")
+        val feels_like: Double
+    )
+
+    data class WeatherDetails(
+        @SerializedName("description")
+        val description: String,
+    )
+}
