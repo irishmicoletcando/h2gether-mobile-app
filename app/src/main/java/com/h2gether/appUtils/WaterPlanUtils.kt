0182dd58f92ed492ca487124f169c5a6236b2118
John Ric Merque <johnricmer@gmail.com>
2023-07-01 11:44:43 +0800

    Feat: Algorithm for vital statistics and weather to construct water plan

--- app/src/main/java/com/h2gether/appUtils/WaterPlanUtils.kt
+++ app/src/main/java/com/h2gether/appUtils/WaterPlanUtils.kt
@@ -5,7 +5,34 @@ class WaterPlanUtils {
     val WeatherUtils = WeatherUtils()
     val UserConfigUtils = UserConfigUtils()
 
-    fun constructTargetWaterPlan(){
-
+    fun calculateTargetWaterLevel(gender: String, age: Int, isModerateWeather: Boolean): Int {
+        return when (gender) {
+            "Female" -> {
+                when {
+                    age in 0..3 -> if (isModerateWeather) 900 else 1700
+                    age in 4..8 -> if (isModerateWeather) 1200 else 2100
+                    age in 9..13 -> if (isModerateWeather) 1600 else 2400
+                    age in 14..18 -> if (isModerateWeather) 1800 else 3100
+                    age in 19..30 -> if (isModerateWeather) 2200 else 3700
+                    age in 31..50 -> if (isModerateWeather) 2200 else 3700
+                    age >= 51 -> if (isModerateWeather) 2200 else 3700
+                    else -> -1 // Invalid age
+                }
+            }
+            "Male" -> {
+                when {
+                    age in 0..3 -> if (isModerateWeather) 900 else 1700
+                    age in 4..8 -> if (isModerateWeather) 1200 else 2100
+                    age in 9..13 -> if (isModerateWeather) 1800 else 2600
+                    age in 14..18 -> if (isModerateWeather) 2600 else 4500
+                    age in 19..30 -> if (isModerateWeather) 3000 else 5100
+                    age in 31..50 -> if (isModerateWeather) 3000 else 5100
+                    age >= 51 -> if (isModerateWeather) 3000 else 5100
+                    else -> -1 // Invalid age
+                }
+            }
+            else -> 2200
+        }
     }
+
 }
