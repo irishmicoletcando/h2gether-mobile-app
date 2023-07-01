b0e0ec84d4b2d7afe666537ad320d9085194ef60
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-07-01 23:43:09 +0800

    Add about us activity

---
+++ app/src/main/java/com/h2gether/settingsPage/AboutUsActivity.kt
@@ -0,0 +1,12 @@
+package com.h2gether.settingsPage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.h2gether.R
+
+class AboutUsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_us)
+    }
+}
