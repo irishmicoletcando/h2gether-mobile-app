57ecd6b7040ac5873a58ec59d05762fdda774b79
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-07-01 18:48:45 +0800

    Feat: Add privacy policy page toolbar

---
+++ app/src/main/java/com/h2gether/settingsPage/privacyPolicyActivity.kt
@@ -0,0 +1,32 @@
+package com.h2gether.settingsPage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import com.example.h2gether.R
+
+class privacyPolicyActivity : AppCompatActivity() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val rootView = inflater.inflate(R.layout.fragment_settings_page, container, false)
+
+        val backButton = rootView.findViewById<ImageButton>(R.id.back_button)
+        val pageTitle = rootView.findViewById<TextView>(R.id.toolbar_title)
+        val logoutButton: ImageButton = rootView.findViewById(R.id.logout_button)
+        logoutButton.visibility = View.GONE
+
+        // Customize the toolbar as needed
+        pageTitle.text = "Privacy Policy"
+        backButton.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
+        return rootView
+    }
+}
