27954bc8b0f1c74bf9c9f9474c0527fad10ffbd2
Ma. Jensen Nicole Dela Rosa <jensennics.dlrs@gmail.com>
2023-07-04 14:44:28 +0800

    feat: Add necessary imports for recycler view

---
+++ app/src/main/java/com/h2gether/homePage/ViewPageAdapter.kt
@@ -0,0 +1,12 @@
+package com.h2gether.homePage
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.h2gether.R
+
+class ViewPageAdapter {
+}
